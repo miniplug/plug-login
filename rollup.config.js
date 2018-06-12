20f8f298127fca3e5c8837ae285146b1bbfa2d84
Renée Kooi <renee@kooi.me>
2018-06-12 21:11:49 +0200

    Experiment: inline small, rarely changing dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
 import builtins from 'builtin-modules'
 
 const pkg = require('./package.json')
 
 export default {
-  input: 'src/index.js',
+  input: './src/index.js',
   output: [
-    { format: 'cjs', file: pkg.main, sourcemap: true, exports: 'default' },
+    { format: 'cjs', file: pkg.main, sourcemap: true, exports: 'default', interop: false },
     { format: 'es', file: pkg.module, sourcemap: true }
   ],
   external: builtins.concat(Object.keys(pkg.dependencies)),
   plugins: [
     buble({
-      include: 'src/**',
+      include: './src/**',
       target: { node: 4 },
       objectAssign: 'Object.assign'
-    })
+    }),
+    resolve(),
+    commonjs({ include: './node_modules/**' })
   ]
 }
